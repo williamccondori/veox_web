66df2ee82ff974ffdb68b1a2a8a6cf51dce1d53d
William Condori Quispe <williamccondori@outlook.com>
2019-04-04 07:14:14 -0500

    Se agrega archivos de coniguracion

--- veox_web/settings.py
+++ veox_web/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'ky2&g$!7y2dh5vxnz6v$hzm*8q8--f)%_^ya%x*vfcsjr)-h#0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
